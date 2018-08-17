b785401d3a1268a71e2046e0d3c4804233f15816
TomasJurk <tomasstj@gmail.com>
2018-08-17 18:40:06 +0300

    Timer with words prompt done, app can be used for learning

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { WordService } from '../word.service';
 
 @Component({
@@ -6,13 +6,76 @@ import { WordService } from '../word.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  time = 30;
+  timerId: any;
+  timerStopped: boolean;
+
+  allWords: any;
+  lastTwo = [];
+  correctAnswer: string;
 
   constructor(
     private _wS: WordService
   ) { }
 
   ngOnInit() {
+    this._wS.getAllWords().valueChanges()
+    .subscribe(data => {
+      console.log(data);
+      if (data.length < 3) {
+        alert('Please add more words, must be at least three');
+      } else {
+        this.allWords = data;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.stopTimer();
+  }
+
+  getOneWord() {
+    const randomIndex = Math.floor(Math.random() * this.allWords.length);
+    if (this.lastTwo.includes(randomIndex)) {
+      this.getOneWord();
+      return;
+    } else {
+      if (this.lastTwo.length === 2) {
+        this.lastTwo.shift();
+        this.lastTwo.push(randomIndex);
+      } else {
+        this.lastTwo.push(randomIndex);
+      }
+      const answer = prompt('Translate: '+ this.allWords[randomIndex].word);
+      if (answer == this.allWords[randomIndex].translation) {
+        this.correctAnswer = 'correct';
+      } else {
+        this.correctAnswer = 'incorrect';
+      }
+      setTimeout(() => {
+        this.correctAnswer = '';
+      }, 5000);
+    }
+    console.log(this.lastTwo);
+  }
+
+  initTimer() {
+    clearInterval(this.timerId);
+    this.getOneWord();
+    this.timerId = setInterval(() => {
+      this.getOneWord();
+    }, this.time*60000);
+  }
+
+  stopTimer() {
+    clearInterval(this.timerId);
+    this.correctAnswer = '';
+    this.timerStopped = true;
+    setTimeout(() => {
+      this.timerStopped = false;
+    }, 5000);
   }
 
 }
