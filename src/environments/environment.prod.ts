4cf12f91132c13dc5124b840b36297102c36486a
TomasJurk <33634105+TomasJurk@users.noreply.github.com>
2018-08-20 11:41:07 +0300

    enviroment.prod changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCCjTmw4_kBgIfhYeQpxx1MrB0UmhHcKdk",
+    authDomain: "my-web-tomasstj.firebaseapp.com",
+    databaseURL: "https://my-web-tomasstj.firebaseio.com",
+    projectId: "my-web-tomasstj",
+    storageBucket: "my-web-tomasstj.appspot.com",
+    messagingSenderId: "503946629658"
+  }
 };
